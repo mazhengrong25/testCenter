f326a6c6447cd593fe5a8d6f2c30c60728c59ed5
wishQAQ <wishtime1024@gmail.com>
2020-08-11 18:00:13 +0800

    测试中心框架搭建

---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,38 @@
+// 压缩css、js
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+// 要压缩的文件
+const productionGzipExtensions = ['js', 'css'];
+// const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+const env = process.env.NODE_ENV;
+
+module.exports = {
+  // 针对请求数进行优化
+  chainWebpack: config => {
+    // 移除 prefetch 插件
+    config.plugins.delete('prefetch')
+    // 移除 preload 插件
+    config.plugins.delete('preload');
+  },
+  // console移除
+  configureWebpack(config) {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: false,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {},
+    // 启用 CSS requireModuleExtension for all css / pre-processor files.
+    requireModuleExtension: false
+  },
+  publicPath: process.env.NODE_ENV === 'production'? './dist/': './',
+  productionSourceMap: false,
+  // devServer: {
+  //   proxy: '',
+  //   changeOrigin: true,
+  // }
+}
